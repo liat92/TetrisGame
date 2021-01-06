024a5bd5752d273dc8ea865ceefb017aa7ea8c51
Liat Matzov <46567772+liat92@users.noreply.github.com>
2021-01-06 16:31:59 +0200

    Update Lshape.cpp
    Fixing documentation

--- Tetris/Source Files/Lshape.cpp
+++ Tetris/Source Files/Lshape.cpp
@@ -6,7 +6,7 @@
 
     DEGREE_0  | DEGREE_90 | DEGREE_180 |  DEGREE_270
         0          2 0         3 1 2           3
-  		2 1 3      1               0		   1
+    2 1 3          1           0	       1
                    3                         0 2
 */
 Lshape::Lshape(int xCord, int yCord, eAngle theAngle)
